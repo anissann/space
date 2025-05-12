0c5935168f2124fed465624e033b4034d42883bd
Anissa <anisa.abdramanova@mail.ru>
2025-05-12 11:52:08 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger').addEventListener('click',function(){
+    document.querySelector('nav').classList.toggle('active');
+      document.querySelector('.burger').classList.toggle('burger-active');
+  
+  });
